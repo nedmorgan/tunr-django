2cc8120167b52a6832194784f8ba5119e238a4f0
nedmorgan <nmorgan1987@gmail.com>
2019-04-05 10:57:35 -0400

    created route and view for individual artist

--- tunr_django/tunr/urls.py
+++ tunr_django/tunr/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.artist_index, name='artist_index'),
     path('songs/', views.song_index, name='song_index'),
+    path('artists/<int:pk>', views.artist_show, name='artist_show'),
 ]
--- tunr_django/tunr/views.py
+++ tunr_django/tunr/views.py
@@ -9,6 +9,11 @@ def artist_index(request):
     return render(request, 'tunr/artist_index.html', context)
 
 
+def artist_show(request, pk):
+    context = {'artist': Artist.objects.get(pk=pk)}
+    return render(request, 'tunr/artist_show.html', context)
+
+
 def song_index(request):
     context = {'songs': Song.objects.all()}
     return render(request, 'tunr/song_index.html', context)
