88354c0ebb37a331f2ca29ff284be65a470974bf
nedmorgan <nmorgan1987@gmail.com>
2019-04-05 12:31:09 -0400

    able to view new artist form

--- tunr_django/tunr/urls.py
+++ tunr_django/tunr/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.artist_index, name='artist_index'),
     path('artists/<int:pk>', views.artist_show, name='artist_show'),
+    path('artists/new', views.artist_new, name='artist_new'),
     path('songs/', views.song_index, name='song_index'),
     path('songs/<int:pk>', views.song_show, name='song_show'),
 ]
--- tunr_django/tunr/views.py
+++ tunr_django/tunr/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Artist, Song
+from .forms import ArtistForm
 
 # Create your views here.
+
+# Artist Functions
 
 
 def artist_index(request):
@@ -12,6 +15,19 @@ def artist_index(request):
 def artist_show(request, pk):
     context = {'artist': Artist.objects.get(pk=pk)}
     return render(request, 'tunr/artist_show.html', context)
+
+
+def artist_new(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            artist = form.save()
+            return redirect('artist_index')
+    else:
+        form = ArtistForm()
+    return render(request, 'tunr/artist_form.html', {'form': form})
+
+# Song Functions
 
 
 def song_index(request):
